040d79c79bf6249428de29e54cdc18e939380763
Amber Kim <ambergkim@gmail.com>
2018-08-09 19:36:36 -0700

    server fix

--- server.js
+++ server.js
@@ -114,9 +114,7 @@ app.use('/api', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname + '/build')));
-
-
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
